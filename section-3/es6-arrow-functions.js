ddf7dee7186b41754666a1f3454c02d335671091
Derek Owusu Bekoe <dowusubekoe@outlook.com>
2021-09-15 07:33:35 -0400

    updated the arrow-function file

--- section-3/es6-arrow-functions.js
+++ section-3/es6-arrow-functions.js
@@ -20,3 +20,26 @@ console.log(findEvenNum);
 const ages = [23, 42, 6, 18];
 const adults = ages.filter(age => age > 20);
 console.log(adults);
+
+
+// Arrow function with ZERO arguments
+let randomNumber2 = () => Math.random;
+console.log(randomNumber2());
+
+// Arrow function with one parameter
+let isPositive2 = number => number >= 0;
+console.log(isPositive2(-1));
+
+
+// Arrow function with two or more parameters
+let sum2 = (a, b) => a + b;
+console.log(sum2(2, 4));
+
+
+// Anonymous Function
+
+const square = number => number * number;
+console.log(square(5));
+
+
+
